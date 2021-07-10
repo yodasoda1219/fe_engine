93647c6f2b7c195ed82a57de4527dd20a886d2c1
yodasoda1219 <theo.github@bedafamily.com>
2021-07-09 17:35:54 -0700

    add dotnet lua script

---
+++ premake/dotnet.lua
@@ -0,0 +1,30 @@
+newoption {
+    trigger = "python-command",
+    value = "CMD",
+    description = "The python interpreter command name/path",
+    default = "python"
+}
+function dotnet_project(path)
+    kind "Utility"
+    files {
+        (path)
+    }
+    filter "files:**.csproj"
+        buildmessage "Compiling .NET Core project: %{file.basename}"
+        buildcommands {
+            'dotnet build "%{!file.abspath}" -o "%{cfg.targetdir}"'
+        }
+        buildoutputs { "%{cfg.targetdir}/%{file.basename}.dll" } -- .NET Core always outputs DLLs
+end
+function generate_dotnet_project(directory_path, name, template, definitions, references, rootdir, scriptdir)
+    local abs_rootdir = path.getabsolute(path.join(scriptdir, rootdir))
+    print(abs_rootdir)
+    local abs_directory = path.getabsolute(path.join(abs_rootdir, directory_path))
+    python_command = _OPTIONS["python-command"]
+    local command = 'bash -c "cd ' .. abs_rootdir .. ' && ' .. python_command .. ' -m scripts create-dotnet-project \\"' .. name .. '\\" \\"' .. abs_directory .. '\\" \\"' .. template .. '\\""'
+    for index in pairs(definitions) do
+        print(definitions[index])
+    end
+    os.execute(command) 
+    return path.join(abs_directory, name) .. ".csproj"
+end
